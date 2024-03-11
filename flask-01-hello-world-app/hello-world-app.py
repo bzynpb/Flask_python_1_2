4dc405d5c72f81fa721582490f75f515b47d3d31
Zeynep <bzbilgic@gmail.com>
2024-03-11 20:42:55 +0000

    “new”

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,22 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route("/") 
+def head():
+    return "hello word clarusway-17"
 
+@app.route("/second") # url sonuna /second yazarsa, belirtilen sayfaya gider
+def head2():
+    return "This is second page."
+
+@app.route("/third") # url sonuna /third yazarsa, belirtilen sayfaya gider
+def head2():
+    return "This is third page."
 
 
 
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    # app.run(debug=True, port=30000)
+    app.run(host= '0.0.0.0', port=8081) # hostta calismasi icin yukaridaki kodu comment yaptik
--- flask-02-Jinja_Template/jinja.py
+++ flask-02-Jinja_Template/jinja.py
@@ -10,5 +10,5 @@ app = Flask(__name__)
 
 
 if __name__== "__main__":
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    # app.run(debug=True, port=30000)
+    app.run(host= '0.0.0.0', port=8081)
